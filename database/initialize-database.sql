84db7e58f46329f6e5aedcbdcca91df476a8e20d
dennismolund <43784882+dennismolund@users.noreply.github.com>
2022-01-19 14:35:23 +0100

    first commit

---
+++ database/initialize-database.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS accounts (\
+    id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    username VARCHAR(50) NOT NULL UNIQUE, \
+    email VARCHAR(50) NOT NULL UNIQUE, \
+    password VARCHAR(50) NOT NULL) \
+
+
+CREATE TABLE IF NOT EXISTS comments (\
+    id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    username VARCHAR(50) NOT NULL, \
+    accountId INTEGER NOT NULL, \
+    message VARCHAR(350) NOT NULL, \
+    sourceCode VARCHAR(350) NOT NULL, \
+    projectID INTEGER NOT NULL, \
+    FOREIGN KEY(projectID) REFERENCES projects(id), \
+    FOREIGN KEY(accountId) REFERENCES accounts(id), \
+    FOREIGN KEY(username) REFERENCES accounts(username))
+
+CREATE TABLE IF NOT EXISTS projects (\
+    id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    username VARCHAR(50) NOT NULL, \
+    accountId INTEGER NOT NULL, \
+    sourceCode VARCHAR(500) NOT NULL, \
+    Title VARCHAR(50) NOT NULL, \
+    Tag VARCHAR(350), \
+    FOREIGN KEY(accountId) REFERENCES accounts(id), \
+    FOREIGN KEY(username) REFERENCES accounts(username))
---
+++ readme.txt
@@ -0,0 +1,4 @@
+"Docker-compose up" will run the latest build of the image (webbapp & database). If you make changes to an image, 
+you need to rebuild it before using docker-compose up, or simply use the command docker-compose up --build.
+
+Nodemon is fully functional for changes in app.js Changes outside of app.js will only be seen after restarting with: docker-compose up -V 
