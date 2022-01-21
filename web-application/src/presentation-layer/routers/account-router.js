1620bec43212fb7585942140dfd113ab0187a65e
dennismolund <43784882+dennismolund@users.noreply.github.com>
2022-01-21 12:47:33 +0100

    add presentation-layer folder and put all relevant files in it

--- web-application/src/presentation-layer/routers/account-router.js
+++ web-application/src/presentation-layer/routers/account-router.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const accountManager = require('../business-logic-layer/account-manager')
+const accountManager = require('../../business-logic-layer/account-manager')
 
 const router = express.Router()
 
