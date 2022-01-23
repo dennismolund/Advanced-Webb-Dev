82e210a98fab8301847f407fb0cae69e36666a64
rol-jacob <jacob.carlquist@rolgroup.com>
2022-01-23 12:37:42 +0100

    Refactor code in bars-router

--- web-application/src/presentation-layer/routers/bars-router.js
+++ web-application/src/presentation-layer/routers/bars-router.js
@@ -7,45 +7,36 @@ module.exports = function({barsManager}){
     const router = express.Router()
 
     router.get("/", (req, res) => {
-        getRunda(req, res, barsManager);
+        barsManager.getBarRunda(req.session.activeAccount, (error, result) => {
+            if (error) {
+                console.log(error);
+                // TODO ?
+            } else if (result){
+                console.log('Found existing barrunda');
+                res.render("home.hbs", {bars: result.list});
+            } else {
+                res.render("home.hbs", {bars: []});
+                // There was no data found
+            }
+        });
     });
 
     router.post('/', (req, res) => {
-        storeBarRunda(req, res, barsManager);
-    });
+        console.log('Creating and storing new barrunda');
+        const barRunda = barlist.getRandom();
 
+        barsManager.storeBarRunda(barRunda, req.session.activeAccount, (error, result) => {
+            if (error) {
+                console.log('Failed to save barrunda');
+                console.log(error);
+                // TODO ?
+            } else {
+                console.log('Succesfully stored new barrunda');
+                // TODO ?
+            }
+        });
+        res.render("home.hbs", {bars: barRunda.list})
+    });
+    
     return router
-}
-
-const getRunda = (req, res, manager) => {
-    manager.getBarRunda(req.session.activeAccount, (error, result) => {
-        if (error) {
-            console.log(error);
-            // TODO ?
-        } else if (result){
-            console.log('Found existing barrunda');
-            res.render("home.hbs", {bars: result.list});
-        } else {
-            res.render("home.hbs", {bars: []});
-            // There was no data found
-        }
-    });
-}
-
-const storeBarRunda = (req, res, manager) => {
-    console.log('Creating and storing new barrunda');
-
-    const barRunda = barlist.getRandom();
-
-    manager.storeBarRunda(barRunda, req.session.activeAccount, (error, result) => {
-        if (error) {
-            console.log('Failed to save barrunda');
-            console.log(error);
-            // TODO ?
-        } else {
-            console.log('Succesfully stored new barrunda');
-            // TODO ?
-        }
-    });
-    res.render("home.hbs", {bars: barRunda.list})
 }
