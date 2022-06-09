3a8a2fb0d0802f28170d0966230d78f7e5efddaa
dennismolund <43784882+dennismolund@users.noreply.github.com>
2022-06-09 15:10:27 +0200

    Delete --save from redis comman in cocker-compose.yml, seems to cause db error when restarting after crash

--- web-application/src/presentation-layer/app.js
+++ web-application/src/presentation-layer/app.js
@@ -71,7 +71,8 @@ app.get('/', async (req, res) => {
     if(req.session.activeAccount) {
       const account = req.session.activeAccount
       //a.logBy('name');
-      if(req.session.activeAccount.team_id) res.redirect('/teams');
+      if(req.session.activeAccount.team_id) {
+        res.redirect('/teams');}
       else if(req.session.activeAccount.pubcrawl_id) res.redirect('/bars');
       else res.render('start.hbs', {activeAccount: account});
       // res.render('home.hbs');
