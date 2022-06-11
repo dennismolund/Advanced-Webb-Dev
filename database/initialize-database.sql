c24d8d39c6a4137c67b948a82e9a523394d9d91f
Jacob Carlquist <jacob.carlquist@rolgroup.com>
2022-06-11 15:55:27 +0200

    Removed on delete cascade

--- database/initialize-database.sql
+++ database/initialize-database.sql
@@ -11,7 +11,7 @@ CREATE TABLE pubcrawl (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     owner_id INT NOT NULL,
     data JSON NOT NULL,
-    FOREIGN KEY (owner_id) REFERENCES account(id) ON DELETE CASCADE
+    FOREIGN KEY (owner_id) REFERENCES account(id)
 );
 
 CREATE TABLE team (
@@ -19,7 +19,7 @@ CREATE TABLE team (
     teamname VARCHAR(50) NOT NULL UNIQUE,
     creator_id INT NOT NULL,
     pubcrawl_id INT,
-    FOREIGN KEY (creator_id) REFERENCES account(id) ON DELETE CASCADE
+    FOREIGN KEY (creator_id) REFERENCES account(id)
 );
 
 ALTER TABLE account ADD CONSTRAINT FOREIGN KEY (pubcrawl_id) REFERENCES pubcrawl(id) ON DELETE SET NULL;
