d76e18f8d6cd769ee075709b310e3e276cfd672c
dennismolund <43784882+dennismolund@users.noreply.github.com>
2022-07-25 12:56:58 +0200

    Close #39 / Add comment explaining hasTeamCheck middleware in REST API

--- web-application/src/presentation-layer-REST-API/middleware/barsMiddlewares.js
+++ web-application/src/presentation-layer-REST-API/middleware/barsMiddlewares.js
@@ -1,3 +1,6 @@
+//This code check if the user has a team. 
+//The REST API does not support team CRUD operations as for grade 3 the REST API only needs to support CRUD operations on two resources: Account and pubcrawl.
+//If the user have joined a team "outside" the REST API the user must leave/delete their team before they can use the functionality of the REST API. 
 const hasTeamCheck = (req, res, next) => {
     if (req.account.team_id) {
         res.status(403).send({
